dd630399ffeaac5aa56aa705c24bc7cea2bbccb0
Matthew Kilgore <m.kilgore0@gmail.com>
2024-05-17 10:12:50 -0400

    Now renders a small grid of ASCII grass

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite, Texture, Ticker } from "pixi.js"
+import { Application, Sprite, Texture, Ticker, Container } from "pixi.js"
 
 // Create the application
 const app = new Application()
@@ -8,22 +8,44 @@ await app.init({ resizeTo: window, background: "#282828", antialias: true })
 // @ts-ignore
 document.body.appendChild(app.canvas)
 
-let canvas = document.createElement('canvas')
-canvas.width = 24
-canvas.height = 24
-let ctx = canvas.getContext("2d")
-ctx.font = "24px serif"
-ctx.fillStyle = "#33FF00"
-ctx.textBaseline = "middle"
-ctx.textAlign = "center"
-ctx.fillText("@", 12, 12)
+class TextSprite {
+    text: string
+    canvas: HTMLCanvasElement
+    ctx: CanvasRenderingContext2D
+    sprite: Sprite
 
-let sprite = new Sprite(Texture.from(canvas))
-app.stage.addChild(sprite)
+    constructor(text: string) {
+        this.text = text
+        this.canvas = document.createElement('canvas')
+        this.canvas.width = 24
+        this.canvas.height = 24
+        this.ctx = this.canvas.getContext("2d")
+        this.ctx.font = "24px serif"
+        this.ctx.fillStyle = "#33FF00"
+        this.ctx.textBaseline = "middle"
+        this.ctx.textAlign = "center"
+        this.ctx.fillText(this.text, 12, 12)
+        this.sprite = new Sprite(Texture.from(this.canvas))
+    }
+}
+
+const ROWS = 10
+const COLS = 10
+
+let background = new Container()
+
+for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+        let sprite = new TextSprite(Math.random() * 2 > 1 ? "\"" : ",")
+        sprite.sprite.position.set(row * 24, col* 24)
+        background.addChild(sprite.sprite)
+    }
+}
+app.stage.addChild(background)
 
 function update(ticker: Ticker) {
-    sprite.x = app.renderer.width / 2
-    sprite.y = app.renderer.height / 2
+    background.x = (app.renderer.width - ROWS * 24) / 2
+    background.y = (app.renderer.height - COLS * 24) / 2
 }
 
 app.ticker.add(update)
